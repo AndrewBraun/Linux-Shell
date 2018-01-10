1f95a84e3e7aba3af05b307928f5c482c805dcff
AndrewBraun <ABandrewbraun@gmail.com>
2018-01-09 16:08:22 -0800

    Add files via upload

---
+++ ssi.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#define MAX_PROMPT_SIZE 128
+/*
+* This function reads input from a user
+* and determines what to do based on it.
+*/
+void parseLine(char* input){
+	return;
+}
+
+int main(){
+	char* prompt[MAX_PROMPT_SIZE];
+	while(1){
+		return 0;
+	}	
+}
