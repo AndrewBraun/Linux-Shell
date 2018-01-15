4f99826df0b1406710676d772c47fa5e943f34d6
AndrewBraun <ABandrewbraun@gmail.com>
2018-01-14 19:18:48 -0800

    Update ssi.c

--- ssi.c
+++ ssi.c
@@ -1,8 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#define MAX_PROMPT_SIZE 128
+//#include <readline/readline.h>
+//#include <readline/history.h>
+#include <unistd.h>
+#include <string.h>
+#define MAX_DIRECTORY_PATH_SIZE 512
+
+/*
+* This function gives the prompt to main.
+* Returns the prompt string, NULL if there's a buffer size error.
+*/
+char *getPrompt(){
+	char currentDirectory[MAX_DIRECTORY_PATH_SIZE];
+	if(getcwd(currentDirectory,MAX_DIRECTORY_PATH_SIZE)==NULL){
+		puts("ERROR: Directory path name is too long.");
+		return NULL;
+	}
+	char *prompt = malloc(sizeof(char)*(MAX_DIRECTORY_PATH_SIZE+7));
+	strncpy(prompt,"SSI: ",5);
+	strncat(prompt,currentDirectory,MAX_DIRECTORY_PATH_SIZE);
+	strncat(prompt," >",2);
+	return prompt;
+}
+
 /*
 * This function reads input from a user
 * and determines what to do based on it.
@@ -11,9 +31,23 @@ void parseLine(char* input){
 	return;
 }
 
+/*
+* This function changes the current directory.
+* It is called with "cd".
+*/
+void changeDirectory(){
+	return;
+}
+
 int main(){
+	/*
 	char* prompt[MAX_PROMPT_SIZE];
 	while(1){
+		
 		return 0;
-	}	
+	}
+	*/
+	char *prompt = getPrompt();
+	puts(prompt);
+	free(prompt);	
 }
